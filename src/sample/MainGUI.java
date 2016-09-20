84efa2c8430d224eb22c9c20ac714b0376dd6c2b
tuantonyd <tuantonyd@gmail.com>
2016-09-20 08:30:57 -0500

    Initial commit

---
+++ src/sample/MainGUI.java
@@ -0,0 +1,18 @@
+package sample;/**
+ * Created by tuan on 9/12/2016.
+ */
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainGUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
